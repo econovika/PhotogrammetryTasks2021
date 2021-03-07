592b5357a2108303c68b8406cb906eff1092b91c
ksenia <kseniyanovikova2008@gmail.com>
2021-03-07 20:27:52 +0300

    Fix merge mistake

--- src/phg/sfm/defines.h
+++ src/phg/sfm/defines.h
@@ -59,9 +59,9 @@ inline void copy(const cv::Matx<double, SIZE, SIZE> &Fcv, EIGEN_TYPE &F)
 }
 
 template <int SIZE, typename EIGEN_TYPE>
-inline void copy(const EIGEN_TYPE &F, cv::Matx<double, SIZE, SIZE>  &Fcv)
-{
+inline void copy(const EIGEN_TYPE &F, cv::Matx<double, SIZE, SIZE>  &Fcv) {
     for (size_t i = 0; i < SIZE; ++i) {
         for (size_t j = 0; j < SIZE; ++j)
             Fcv(i, j) = F(i, j);
     }
+}
